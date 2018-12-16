c85efaaff09963215c22dabd31db7104f399780a
Maxim <maxim.kohan6257@gmail.com>
2018-12-16 15:51:13 +0200

    Updates

---
+++ precache-manifest.86f086a7175fbd2d066f8da016e1c71d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "8009759f7165760b49e2",
+    "url": "/react-app/static/js/main.8009759f.chunk.js"
+  },
+  {
+    "revision": "847415285e321ec38f8e",
+    "url": "/react-app/static/js/1.84741528.chunk.js"
+  },
+  {
+    "revision": "8009759f7165760b49e2",
+    "url": "/react-app/static/css/main.b774116e.chunk.css"
+  },
+  {
+    "revision": "1d6f441baabfc0f10076f3c268556516",
+    "url": "/react-app/index.html"
+  }
+];
